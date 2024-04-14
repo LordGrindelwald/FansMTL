84c4734b8b48b94a96fcaef84cbedcaf2a30ff5e
Ian Grindelwald <164880317+LordGrindelwald@users.noreply.github.com>
2024-04-14 19:36:36 +0530

    Update FansMTL.lua

--- src/en/FansMTL.lua
+++ src/en/FansMTL.lua
@@ -63,7 +63,7 @@ local GENRES = {
 
 return Require("ReadWN")("https://www.fansmtl.com", {
     id = 1308639967,
-    name = "FansMTL",
+    name = "×FansMTL",
     shrinkURLNovel = "^.-fansmtl%.com",
     hasCloudFlare = true,
 
